a3ba75d4813a29263ea27ea3d77194941a3a2a56
phfriedl <phfriedl@ethz.ch>
2018-11-13 15:40:42 +0000

    some drawing improvements

--- Utils/topo_helper.py
+++ Utils/topo_helper.py
@@ -12,80 +12,117 @@ import matplotlib.pyplot as plt
 import re
 
 # CONSTANTS
-EXTERN_X = -1
-FIREWALL_X = 0
-INTERNAL_X = 1
-SERVER_X = 0.5
 HORIZONTAL_SEPARATION = 1
 VERTICAL_SEPARATION = 1
+RE_TO_COMPONENT = {
+    r"fir": 'firewalls',
+    r"hi": 'internal_hosts',
+    r"he": 'external_hosts',
+    r"ser": 'servers'
+}
+COMPONENT_TO_PARAMS = {
+    'external_hosts': {
+        'x': -1,
+        'shape': 's',
+        'color': 'b',
+    },
+    'internal_hosts': {
+        'x': 1,
+        'shape': 'o',
+        'color': 'g',
+    },
+    'servers': {
+        'x': 1,
+        'shape': 'v',
+        'color': 'g'
+    },
+    'firewalls': {
+        'x': 0,
+        'shape': 'd',
+        'color': 'r'
+    }
+}
+
+# HELPERS
+def is_internal_type(type):
+    return (type == 'internal_hosts' or type == 'servers')
+
+def other_internal_type(type):
+    if type == 'internal_hosts':
+        return 'servers'
+    else:
+        return 'internal_hosts'
 
 class TopoHelper(object):
     def __init__(self, topo_db):
         self.topo = Topology(db=topo_db)
-        self.components = {
-            'external_hosts': [],
-            'internal_hosts': [],
-            'servers': [],
-            'firewalls': []
-        }
+        self.components = dict((type, []) for type in COMPONENT_TO_PARAMS.keys())
+        #self.positions = dict((type, {}) for type in COMPONENT_TO_PARAMS.keys())
         self.positions = {}
         self.labels = {}
         self.init()
 
     def init(self):
-        self.re_to_component = {
-            r"fir": 'firewalls',
-            r"hi": 'internal_hosts',
-            r"he": 'external_hosts',
-            r"ser": 'servers'
-        }
-        self.prepare()
-        self.position_nodes()
+        self.get_components()
+        self.set_component_params()
 
     def sort_component(self, switch):
-        for regex, component in self.re_to_component.items():
+        for regex, component in RE_TO_COMPONENT.items():
             if (re.match(regex, switch)):
                 self.components[component].append(switch)
 
-
     # gets all components and seperates them into external, internal, server and firewall
-    def prepare(self):
+    def get_components(self):
         for switch in self.topo.get_p4switches().keys():
             self.sort_component(switch)
             for host in self.topo.get_hosts_connected_to(switch):
                 self.sort_component(host)
 
     # place nodes vertically at position x, and label it
-    def place_nodes(self, nodes, x_pos, additional_vertical_shift=0):
-        shift = (len(nodes)//2)*VERTICAL_SEPARATION + additional_vertical_shift
+    def set_node_params(self, nodes, type):
+        p = COMPONENT_TO_PARAMS[type]
         it = 0
+
+        # special case: position severs and internal nodes above each other
+        if is_internal_type(type):
+            other_type = other_internal_type(type)
+            other_internals = self.components[other_type]
+            combined_length = len(nodes + other_internals)
+            shift = (combined_length//2)*VERTICAL_SEPARATION
+            if self.components[other_type][0] in self.positions:
+                # other internal type already seen
+                it = it + len(other_internals)
+        else:
+            shift = (len(nodes)//2)*VERTICAL_SEPARATION
+
         for node in nodes:
-            self.positions[node] = (HORIZONTAL_SEPARATION*x_pos, shift-it)
+            self.positions[node] = (HORIZONTAL_SEPARATION*p['x'], shift-it)
             self.labels[node] = node
             it = it + 1
 
-    # positions nodes according to their type
-    def position_nodes(self):
+    # position nodes according to their type
+    def set_component_params(self):
         for type, components in self.components.items():
-            if type == 'external_hosts':
-                self.place_nodes(components, EXTERN_X)
-            elif type == 'internal_hosts':
-                self.place_nodes(components, INTERNAL_X)
-            elif type == 'servers':
-                self.place_nodes(components, SERVER_X, additional_vertical_shift=0.2)
-            elif type == 'firewalls':
-                self.place_nodes(components, FIREWALL_X)
-
+            self.set_node_params(components, type)
 
     def draw(self):
+        G = self.topo.network_graph
         print('Drawing...', end='')
         """ DEBUG """
         # print(self.components)
         # print(self.topo.network_graph.nodes())
         # print(self.positions)
         """ END DEBUG """
+        for type, components in self.components.items():
+            params = COMPONENT_TO_PARAMS[type]
+            nx.draw_networkx_nodes(G, self.positions,
+                node_shape = params['shape'],
+                node_color = params['color'],
+                nodelist = components
+            )
+        nx.draw_networkx_edges(G, self.positions)
 
-        nx.draw(self.topo.network_graph, pos=self.positions, labels=self.labels)
+        #nx.draw_networkx(self.topo.network_graph, pos=self.positions, labels=self.labels)
         plt.show()
         print('done!')
 
