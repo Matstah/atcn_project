a9558ed43d6f70b35fd39f84bb7515a9c7592e94
phfriedl <phfriedl@ethz.ch>
2018-11-14 10:08:05 +0000

    send script: provide dst by name or ip

--- Project/testing/send.py
+++ Project/testing/send.py
@@ -5,10 +5,17 @@ import sys
 import socket
 import random
 import struct
+from subprocess import Popen, PIPE
+import logging as log
 
+from p4utils.utils.topology import Topology
 from scapy.all import sendp, get_if_list, get_if_hwaddr
 from scapy.all import Ether, IP, UDP, TCP
 import time
+import re
+
+# GLOBALS
+topo=None
 
 def get_if():
     ifs=get_if_list()
@@ -18,19 +25,36 @@ def get_if():
             iface=i
             break;
     if not iface:
-        print "Cannot find eth0 interface"
+        print("Cannot find eth0 interface")
         exit(1)
     return iface
 
+def get_dst_mac(ip):
+    try:
+        pid = Popen(["arp", "-n", ip], stdout=PIPE)
+        s = pid.communicate()[0]
+        mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
+        return mac
+    except:
+        return None
+
+# resolves host to ipv4 and checks first if
+def get_host_ip(host):
+    if not re.match(r'(\d{1,3}\.){3}\d{1,3}', host):
+        return topo.get_host_ip(host)
+    else:
+        return socket.gethostbyname(host)
+
 def send(dst, packets):
-    print('Sending {} packets to {}'.format(packets, dst))
-    addr = socket.gethostbyname()
+    log.debug('Sending {} packets to {}'.format(packets, dst))
+    addr = get_host_ip(dst)
     iface = get_if()
-    print("sending on interface %s to %s" % (iface, str(addr)))
+    log.info("sending on interface %s to %s" % (iface, str(addr)))
 
     for _ in range(packets):
         pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-        #pkt = pkt / IP(dst=addr) / TCP(dport=7777, sport)
+        pkt = pkt / IP(dst=addr) / TCP(dport=80, sport=random.randint(49152,65535))
+        sendp(pkt, iface=iface, verbose=False)
 
 
 if __name__ == "__main__":
@@ -38,19 +62,35 @@ if __name__ == "__main__":
 
     # args for local execution
     parser.add_argument('--local', action='store_true', required=False, help='If script should run from local prompt')
-    parser.add_argument('--src', type=str, required=False, help='Source host in case of a local call')
+    parser.add_argument('--src', type=str, required=False, help='Source host NAME in case of a local call')
     parser.add_argument('--on_remote', action='store_true', required=False, help='Do not set this flag yourself!!')
 
     # args for actual functionality
-    parser.add_argument('-d', '--dst',     type=str, required=True, help='Destination name')
-    parser.add_argument('-p', '--packets', type=int, required=True, help='Number of packets')
+    parser.add_argument('-d', '--dst',     type=str, required=True, help='Destination NAME or IPv4')
+    parser.add_argument('-p', '--packets', type=int, required=False, default=1, help='Number of packets')
+
+    # other args
+    parser.add_argument('--debug', action='store_true', required=False, help='Activate debug messages')
+
+    # parse arguments
     args = parser.parse_args()
 
+    # configure debugger
+    if(args.debug):
+        log.basicConfig(stream=sys.stderr, level=log.DEBUG)
+    else:
+        log.basicConfig(stream=sys.stderr, level=log.INFO)
+
+    # start sending from host or dispatch to host
     if (args.local and not args.on_remote):
         # call script on host with same params, plus on_remote flag to avoid loop
         from subprocess import call
         cmd = ["mx", args.src, 'python'] + sys.argv + ['--on_remote']
-        print("Run the following command:\n{}".format(cmd))
+        log.debug("Run the following command:\n{}".format(cmd))
         call(cmd)
     else:
+        # load globals
+        topo = Topology(db="/home/p4/atcn-project/Project/topology.db")
+
         send(args.dst, args.packets)
+# END MAIN
